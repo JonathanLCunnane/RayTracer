d0aa729055b3ad7c99ac82f7414a92160e5fdb9b
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-01 16:03:57 +0100

    Added equivalence method.

--- src/Tuple.java
+++ src/Tuple.java
@@ -21,4 +21,14 @@ public class Tuple {
     {
         return w == 0;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof Tuple t)) return false;
+        return Double.compare(t.x, x) == 0
+            && Double.compare(t.y, y) == 0
+            && Double.compare(t.z, z) == 0
+            && Double.compare(t.w, w) == 0;
+    }
 }
