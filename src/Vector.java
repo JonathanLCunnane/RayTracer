f3bdaaf2613fdeb954f78092aaa0eac981d00b27
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-01 17:29:52 +0100

    Added methods for finding the magnitude and normal vector of a vector.

--- src/Vector.java
+++ src/Vector.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Vector extends Tuple {
     public Vector(double X, double Y, double Z)
     {
@@ -17,5 +19,16 @@ public class Vector extends Tuple {
     public Vector scalarDivide(double scalar)
     {
         return new Vector(x / scalar, y / scalar, z / scalar);
+    }
+
+    public double magnitude()
+    {
+        return Math.pow((Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)), 0.5);
+    }
+
+    public Vector normalised()
+    {
+        double mag = this.magnitude();
+        return this.scalarDivide(mag);
     }
 }
