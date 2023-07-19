2c7328b104ffbf0bfd867b616f9e497d0b8c4ee4
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-19 20:48:50 +0100

    Added 'times' methods for multiplying a matrix by either a matrix or a tuple. Also fixed a critical error in the initialiseMatrix method.

--- src/Matrix.java
+++ src/Matrix.java
@@ -32,8 +32,18 @@ public class Matrix {
     {
         for (int r = 0; r < rows; r++)
         {
-            System.arraycopy(values, (r * rows), matrix[r], 0, columns);
+            System.arraycopy(values, (r * columns), matrix[r], 0, columns);
         }
+    }
+
+    private Tuple toTuple(Matrix m) // This method will only be used privately, so is unprotected.
+    {
+        return new Tuple(
+                m.valueAt(0, 0),
+                m.valueAt(1, 0),
+                m.valueAt(2, 0),
+                m.valueAt(3, 0)
+        );
     }
 
     public double valueAt(int row, int column)
@@ -44,5 +54,37 @@ public class Matrix {
     public void setAt(int row, int column, double value)
     {
         matrix[row][column] = value;
+    }
+
+    public Matrix times(Matrix m)
+    {
+        if (columns != m.rows)
+        {
+            throw new IllegalArgumentException("Matrices with these dimensions cannot be multiplied.");
+        }
+        Matrix result = new Matrix(rows, m.columns);
+        double currVal;
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < m.columns; c++)
+            {
+                currVal = 0;
+                for (int evalCounter = 0; evalCounter < m.rows; evalCounter++)
+                {
+                    currVal += matrix[r][evalCounter] * m.matrix[evalCounter][c];
+                }
+                result.setAt(r, c, currVal);
+            }
+        }
+        return result;
+    }
+
+    public Tuple times(Tuple t)
+    {
+        double[] v = new double[]
+                {
+                        t.x, t.y, t.z, t.w
+                };
+        return toTuple(times(new Matrix(4, 1, v)));
     }
 }
