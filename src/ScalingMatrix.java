0b8c105923b91f23deb7337390576242ae6e52be
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-22 20:51:07 +0100

    Created a class for scaling matrices.

---
+++ src/ScalingMatrix.java
@@ -0,0 +1,11 @@
+public class ScalingMatrix extends Matrix{
+    public ScalingMatrix(double x, double y, double z)
+    {
+        super(4, 4);
+        double[] v = new double[]
+                {
+                        x, 0, 0, 0, 0, y, 0, 0, 0, 0, z, 0, 0, 0, 0, 1
+                };
+        initialiseMatrix(v);
+    }
+}
