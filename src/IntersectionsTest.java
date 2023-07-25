501920e060bc7756b5bb687f0348542ffbfa9b77
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-25 18:59:53 +0100

    Added tests for the new hit method.

--- src/IntersectionsTest.java
+++ src/IntersectionsTest.java
@@ -18,4 +18,62 @@ public class IntersectionsTest {
                 () -> Assertions.assertEquals(xss.intersections[1].time, 2)
         );
     }
+
+    @Test
+    @DisplayName("The hit method should work with intersections all with positive time.")
+    public void allPosIntersections()
+    {
+        Sphere s = new Sphere();
+        Intersection iOne = new Intersection(s, 1);
+        Intersection iTwo = new Intersection(s, 2);
+        Intersections xs = new Intersections(
+                new Intersection[] { iTwo, iOne }
+        );
+        Intersection i = xs.hit();
+        Assertions.assertEquals(i, iOne);
+    }
+
+    @Test
+    @DisplayName("The hit method should work with intersections where some have positive/negative time.")
+    public void posNegIntersections()
+    {
+        Sphere s = new Sphere();
+        Intersection iOne = new Intersection(s, -1);
+        Intersection iTwo = new Intersection(s, 1);
+        Intersections xs = new Intersections(
+                new Intersection[] { iTwo, iOne }
+        );
+        Intersection i = xs.hit();
+        Assertions.assertEquals(i, iTwo);
+    }
+
+    @Test
+    @DisplayName("The hit method should return null with intersections where all have negative time.")
+    public void allNegIntersections()
+    {
+        Sphere s = new Sphere();
+        Intersection iOne = new Intersection(s, -2);
+        Intersection iTwo = new Intersection(s, -1);
+        Intersections xs = new Intersections(
+                new Intersection[] { iTwo, iOne }
+        );
+        Intersection i = xs.hit();
+        Assertions.assertNull(i);
+    }
+
+    @Test
+    @DisplayName("The hit method should return the lowest non-negative intersection with differently signed times of intersection.")
+    public void variousIntersections()
+    {
+        Sphere s = new Sphere();
+        Intersection iOne = new Intersection(s, 5);
+        Intersection iTwo = new Intersection(s, 7);
+        Intersection iThree = new Intersection(s, -3);
+        Intersection iFour = new Intersection(s, 2);
+        Intersections xs = new Intersections(
+                new Intersection[] { iOne, iTwo, iThree, iFour }
+        );
+        Intersection i = xs.hit();
+        Assertions.assertEquals(i, iFour);
+    }
 }
