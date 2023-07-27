2c897422b6b48dd08e38152f851269fc617eab78
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-27 22:19:20 +0100

    Added an object to represent a point light source.

---
+++ src/PointLight.java
@@ -0,0 +1,9 @@
+public class PointLight {
+    Point position;
+    Colour intensity;
+    public PointLight(Point pos, Colour _intensity)
+    {
+        position = pos;
+        intensity = _intensity;
+    }
+}
