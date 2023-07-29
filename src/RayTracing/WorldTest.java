2808048f7c3e2b5305b37e4e9f7a3a633717a766
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-29 18:52:21 +0100

    Added tests for the shadeHit and colourAt methods.

--- src/RayTracing/WorldTest.java
+++ src/RayTracing/WorldTest.java
@@ -5,6 +5,7 @@ import Matrices.Matrix;
 import Matrices.ScalingMatrix;
 import RayTracing.Objects.Sphere;
 import Tuples.Point;
+import Tuples.Vector;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,5 +47,65 @@ public class WorldTest {
                 () -> Assertions.assertTrue(w.contains(sOne)),
                 () -> Assertions.assertTrue(w.contains(sTwo))
         );
+    }
+
+    @Test
+    @DisplayName("Shade on hit function outside an object")
+    public void shadeHitOutside()
+    {
+        World w = new DefaultWorld();
+        Ray r = new Ray(
+                new Point(0, 0, -5),
+                new Vector(0, 0, 1)
+        );
+        Intersection i = new Intersection(w.objects[0], 4);
+        Computations c = i.prepareComputations(r);
+        Colour lighting = w.shadeHit(c);
+        Assertions.assertEquals(lighting, new Colour(0.38066, 0.47583, 0.2855));
+    }
+
+    @Test
+    @DisplayName("Shade on hit function inside an object")
+    public void shadeHitInside()
+    {
+        World w = new DefaultWorld();
+        w.light = new PointLight(
+                new Point(0, 0.25, 0),
+                new Colour(1, 1, 1)
+        );
+        Ray r = new Ray(
+                new Point(0, 0, 0),
+                new Vector(0, 0, 1)
+        );
+        Intersection i = new Intersection(w.objects[1], 0.5);
+        Computations c = i.prepareComputations(r);
+        Colour lighting = w.shadeHit(c);
+        Assertions.assertEquals(lighting, new Colour(0.90498, 0.90498, 0.90498));
+    }
+
+    @Test
+    @DisplayName("Colour at a certain ray using colourAt when a ray misses")
+    public void rayMisses()
+    {
+        World w = new DefaultWorld();
+        Ray r = new Ray(
+                new Point(0, 0, -5),
+                new Vector(0, 1, 0)
+        );
+        Colour c = w.colourAt(r);
+        Assertions.assertEquals(c, new Colour(0, 0, 0));
+    }
+
+    @Test
+    @DisplayName("Colour at a certain ray using colourAt when a ray hits")
+    public void rayHits()
+    {
+        World w = new DefaultWorld();
+        Ray r = new Ray(
+                new Point(0, 0, -5),
+                new Vector(0, 0, 1)
+        );
+        Colour c = w.colourAt(r);
+        Assertions.assertEquals(c, new Colour(0.38066, 0.47583, 0.2855));
     }
 }
