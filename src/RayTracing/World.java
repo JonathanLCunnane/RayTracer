463ef1683e1e9780c515dd84ab000664db61a097
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-29 18:51:57 +0100

    Added the shadeHit and colourAt methods to simplify the process of finding what the effective colour is at a point where a ray intersects an object.

--- src/RayTracing/World.java
+++ src/RayTracing/World.java
@@ -1,5 +1,6 @@
 package RayTracing;
 
+import Display.Colour;
 import RayTracing.Objects.RayTracerObject;
 
 import java.util.Objects;
@@ -37,5 +38,24 @@ public class World {
             xs = xs.combine(object.intersections(r));
         }
         return xs;
+    }
+
+    public Colour shadeHit(Computations c)
+    {
+        return c.object.getMaterial().lightningAtPoint(
+                light,
+                c.point,
+                c.eyeV,
+                c.normalV
+        );
+    }
+
+    public Colour colourAt(Ray r)
+    {
+        Intersections xs = intersections(r);
+        Intersection i = xs.hit();
+        if (i == null) return new Colour(0, 0, 0); // Black is returned if there is no object intersection
+        Computations c = i.prepareComputations(r);
+        return shadeHit(c);
     }
 }
