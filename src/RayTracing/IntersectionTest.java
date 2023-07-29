ceb546e2bb325b061410d33a65fd16934f468621
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-29 16:03:32 +0100

    Added tests for the new prepareComputations method.

--- src/RayTracing/IntersectionTest.java
+++ src/RayTracing/IntersectionTest.java
@@ -2,6 +2,8 @@ package RayTracing;
 
 import RayTracing.Objects.Sphere;
 
+import Tuples.Point;
+import Tuples.Vector;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,48 @@ public class IntersectionTest {
         Assertions.assertAll(
                 () -> Assertions.assertEquals(i.time, 3.5),
                 () -> Assertions.assertEquals(i.object, s)
+        );
+    }
+
+    @Test
+    @DisplayName("Computations should be correctly prepared and stored for an outside ray intersection")
+    public void outsideIntersection()
+    {
+        Ray r = new Ray(
+                new Point(0, 0, -5),
+                new Vector(0, 0, 1)
+        );
+        Sphere s = new Sphere();
+        Intersection i = new Intersection(s, 4);
+        Computations c = i.prepareComputations(r);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(c.time, i.time),
+                () -> Assertions.assertEquals(c.object, i.object),
+                () -> Assertions.assertEquals(c.point, new Point(0, 0, -1)),
+                () -> Assertions.assertEquals(c.eyeV, new Vector(0, 0, -1)),
+                () -> Assertions.assertEquals(c.normalV, new Vector(0, 0, -1)),
+                () -> Assertions.assertFalse(c.inside)
+        );
+    }
+
+    @Test
+    @DisplayName("Computations should be correctly prepared and stored for an inside ray intersection")
+    public void insideIntersection()
+    {
+        Ray r = new Ray(
+                new Point(0, 0, 0),
+                new Vector(0, 0, 1)
+        );
+        Sphere s = new Sphere();
+        Intersection i = new Intersection(s, 1);
+        Computations c = i.prepareComputations(r);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(c.time, i.time),
+                () -> Assertions.assertEquals(c.object, i.object),
+                () -> Assertions.assertEquals(c.point, new Point(0, 0, 1)),
+                () -> Assertions.assertEquals(c.eyeV, new Vector(0, 0, -1)),
+                () -> Assertions.assertEquals(c.normalV, new Vector(0, 0, -1)),
+                () -> Assertions.assertTrue(c.inside)
         );
     }
 }
