27bb098c98c3d49d49b1a6b4ddb545b4040783e2
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-30 20:40:29 +0100

    Added a variable overPoint which is a point slightly in the direction of the normal to be used when calculating shadows, to prevent shadow 'acne' due to floating point arithmetic.

--- src/RayTracing/Computations.java
+++ src/RayTracing/Computations.java
@@ -8,6 +8,7 @@ public class Computations {
     public double time;
     public RayTracerObject object;
     public Point point;
+    public Point overPoint; // This point is just above the surface with point, and is used to for shadowing purposes.
     public Vector eyeV;
     public Vector normalV;
     public boolean inside; // Whether the ray meets the surface inside the object or not.
@@ -27,5 +28,6 @@ public class Computations {
             normalV = nV;
             inside = false;
         }
+        overPoint = new Point(point.plus(normalV.scalarMultiply(0.00001)));
     }
 }
