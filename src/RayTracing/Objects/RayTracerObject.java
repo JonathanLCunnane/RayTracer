8188a64467e17796114b2a6a3d5b9d071f8a9f60
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-07-30 21:00:31 +0100

    Added implementation notes for making new Objects in the future.

--- src/RayTracing/Objects/RayTracerObject.java
+++ src/RayTracing/Objects/RayTracerObject.java
@@ -8,10 +8,14 @@ import Tuples.Point;
 import Tuples.Vector;
 
 public interface RayTracerObject {
-    Intersections intersections(Ray ray);
+    Intersections intersections(Ray ray); // Ensure the ray is transformed by the inverse of the Object transform, then
+                                          // calculate the intersections with the unit object.
     void setTransform(Matrix m);
     Matrix getTransform();
     void setMaterial(Material mat);
     Material getMaterial();
-    Vector normalAt(Point worldPoint);
+    Vector normalAt(Point worldPoint); // Ensure that the worldPoint is transformed by the inverse of the Object
+                                       // transform, then the local normal is found, and it is then multiplied by the
+                                       // transposed inverse of the Object transform to find the worldNormal. Ensure
+                                       // that worldNormal.w is set to = 0, as translations will change this value.
 }
