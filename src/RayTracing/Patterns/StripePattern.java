b26e27248185026136b2ec75468f0cb02fc13950
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-08-03 14:41:38 +0100

    Created a stripe pattern which alternates in x and is constant in y and z.

---
+++ src/RayTracing/Patterns/StripePattern.java
@@ -0,0 +1,37 @@
+package RayTracing.Patterns;
+
+import Display.Colour;
+import Tuples.Point;
+
+import java.util.Objects;
+
+public class StripePattern extends ParentPattern{
+    public ParentPattern a;
+    public ParentPattern b;
+    public StripePattern(ParentPattern patternA, ParentPattern patternB)
+    {
+        a = patternA;
+        b = patternB;
+    }
+
+    public StripePattern(Colour colourA, Colour colourB)
+    {
+        a = new SolidPattern(colourA);
+        b = new SolidPattern(colourB);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StripePattern that = (StripePattern) o;
+        return Objects.equals(a, that.a) && Objects.equals(b, that.b);
+    }
+
+    public Colour localColourAt(Point localPoint)
+    {
+        if (Math.floor(localPoint.x) % 2 == 0) return a.localColourAt(localPoint);
+        return b.localColourAt(localPoint);
+    }
+}
