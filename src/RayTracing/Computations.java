78988150d7bfe8204964a64e58acfa2545c0bd37
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-08-08 15:33:41 +0100

    Added variables nOne and nTwo to represent the refractive indices of the material a ray is leaving (nOne) and the material a ray is entering (nTwo).

--- src/RayTracing/Computations.java
+++ src/RayTracing/Computations.java
@@ -8,12 +8,15 @@ public class Computations {
     public double time;
     public ParentObject object;
     public Point point;
-    public Point overPoint; // This point is just above the surface with point, and is used to for shadowing purposes.
+    public Point overPoint; // This point is just above the surface with point, and is used to for shadowing and reflection purposes.
+    public Point underPoint; // This point is just below the surface with point, and is used for refraction purposes.
     public Vector eyeV;
     public Vector normalV;
     public Vector reflectV;
     public boolean inside; // Whether the ray meets the surface inside the object or not.
-    public Computations(double t, ParentObject obj, Point p, Vector eV, Vector nV, Vector rV)
+    public double nOne;
+    public double nTwo;
+    public Computations(double t, ParentObject obj, Point p, Vector eV, Vector nV, Vector rV, double nO, double nT)
     {
         time = t;
         object = obj;
@@ -30,6 +33,9 @@ public class Computations {
             inside = false;
         }
         overPoint = new Point(point.plus(normalV.scalarMultiply(0.00001)));
+        underPoint = new Point(point.minus(normalV.scalarMultiply(0.00001)));
         reflectV = rV;
+        nOne = nO;
+        nTwo = nT;
     }
 }
