6d17e835982c032e308f949f1183d78ed7f6edfe
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-08-08 15:32:06 +0100

    Added a method for preparing computations for refraction calculations, setting nOne and nTwo to correct values.

--- src/RayTracing/Intersection.java
+++ src/RayTracing/Intersection.java
@@ -4,6 +4,7 @@ import RayTracing.Objects.ParentObject;
 import Tuples.Point;
 import Tuples.Vector;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Intersection {
@@ -24,12 +25,54 @@ public class Intersection {
         return i.time == time;
     }
 
-    public Computations prepareComputations(Ray r)
+    public Computations prepareComputations(Ray r) // Computations where there is no refraction wanted.
     {
         Point p = new Point(r.position(time));
         Vector nV = object.normalAt(p); // Normal vector
         Vector eV = r.direction.normalised().scalarMultiply(-1); // Eye vector
         Vector rV = r.direction.reflect(nV);
-        return new Computations(time, object, p, eV, nV, rV);
+        return new Computations(time, object, p, eV, nV, rV, 1, 1);
+    }
+
+    public Computations prepareRefractionComputations(Ray r, Intersections xs)
+    {
+        double nOne = 1;
+        double nTwo = 1;
+        boolean isThis = false;
+        ArrayList<ParentObject> containers = new ArrayList<>();
+        for (Intersection i : xs.intersections)
+        {
+            if (i.equals(this))
+            {
+                isThis = true;
+                if (!containers.isEmpty())
+                {
+                    nOne = containers.get(containers.size() - 1).material.refractiveIndex;
+                }
+            }
+
+            if (containers.contains(i.object))
+            {
+                containers.remove(i.object);
+            }
+            else
+            {
+                containers.add(i.object);
+            }
+
+            if (isThis)
+            {
+                if (!containers.isEmpty())
+                {
+                    nTwo = containers.get(containers.size() - 1).material.refractiveIndex;
+                }
+                break;
+            }
+        }
+        Point p = new Point(r.position(time));
+        Vector nV = object.normalAt(p); // Normal vector
+        Vector eV = r.direction.normalised().scalarMultiply(-1); // Eye vector
+        Vector rV = r.direction.reflect(nV);
+        return new Computations(time, object, p, eV, nV, rV, nOne, nTwo);
     }
 }
