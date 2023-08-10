9c7819902046def30daceaf6a985ccb0d90e302c
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-08-10 13:31:47 +0100

    Added basic refraction functionality. Currently, TIR returns a black colour, this needs to be fixed.

--- src/RayTracing/World.java
+++ src/RayTracing/World.java
@@ -54,7 +54,8 @@ public class World {
                 c.object
         );
         Colour reflectedColour = reflectedColour(c, remainingIterations);
-        return surfaceColour.plus(reflectedColour);
+        Colour refractedColour = refractedColour(c, remainingIterations);
+        return surfaceColour.plus(reflectedColour).plus(refractedColour);
     }
 
     public Colour colourAt(Ray r, int remainingIterations)
@@ -62,7 +63,7 @@ public class World {
         Intersections xs = intersections(r);
         Intersection i = xs.hit();
         if (i == null) return new Colour(0, 0, 0); // Black is returned if there is no object intersection
-        Computations c = i.prepareComputations(r);
+        Computations c = i.prepareRefractionComputations(r, xs);
         return shadeHit(c, remainingIterations);
     }
 
@@ -76,6 +77,22 @@ public class World {
         );
         Colour colour = colourAt(reflectedRay, remainingIterations - 1);
         return colour.scalarMultiply(c.object.material.reflectiveness);
+    }
+
+    public Colour refractedColour(Computations c, int remainingIterations)
+    {
+        if (remainingIterations <= 0) return new Colour(0, 0, 0);
+        if (c.object.material.transparency == 0) return new Colour(0, 0, 0);
+        // From Snell's Law, nOne * sinI = nTwo * sinT
+        double nRatio = c.nOne / c.nTwo;
+        double cosI = c.eyeV.dot(c.normalV);
+        double sinTSquared = Math.pow(nRatio, 2) * (1 - Math.pow(cosI, 2));
+        if (sinTSquared > 1) return new Colour(0, 0, 0); // Total internal reflection.
+        double cosT = Math.sqrt(1 - sinTSquared);
+        Vector direction = new Vector(c.normalV.scalarMultiply((nRatio * cosI) - cosT).minus(c.eyeV.scalarMultiply(nRatio)));
+        Ray refractedRay = new Ray(c.underPoint, direction);
+        Colour colour = colourAt(refractedRay, remainingIterations - 1);
+        return colour.scalarMultiply(c.object.material.transparency);
     }
 
     public boolean isInShadow(Point p)
