eb6073575f83f250832ad2e887d9ba58c1e138a1
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-08-10 14:21:47 +0100

    Added a method to calculate the Schlick factor.

--- src/RayTracing/Computations.java
+++ src/RayTracing/Computations.java
@@ -38,4 +38,21 @@ public class Computations {
         nOne = nO;
         nTwo = nT;
     }
+
+    public double schlick()
+    // https://graphics.stanford.edu/courses/cs148-10-summer/docs/2006--degreve--reflection_refraction.pdf
+    // for calculating the amount of light which is reflected/refracted on incidence with a surface which is both
+    // reflective and transparent. This is an approximation of the Fresnel factor.
+    {
+        double cos = eyeV.dot(normalV);
+        if (nOne > nTwo) // If TIR can occur.
+        {
+            double nRatio = nOne / nTwo;
+            double sinTSquared = Math.pow(nRatio, 2) * (1 - Math.pow(cos, 2));
+            if (sinTSquared > 1) return 1; // 100% of rays are reflected if TIR occurs.
+            cos = Math.sqrt(1 - sinTSquared);
+        }
+        double rZero = Math.pow((nOne - nTwo)/(nOne + nTwo), 2);
+        return rZero + ((1 - rZero) * Math.pow(1 - cos, 5));
+    }
 }
