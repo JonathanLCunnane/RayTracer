1ce234667490a7563f22b8b74f238a4f3e1b8480
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-08-11 14:19:59 +0100

    Changed the properties of the glass sphere.

--- src/RayTracing/Objects/GlassSphere.java
+++ src/RayTracing/Objects/GlassSphere.java
@@ -4,7 +4,13 @@ public class GlassSphere extends Sphere{
     public GlassSphere()
     {
         super();
-        material.transparency = 1;
+        material.transparency = 0.9;
+        material.reflectiveness = 0.9;
         material.refractiveIndex = 1.52;
+        material.diffuse = 0.1;
+        material.ambient = 0.1;
+        material.specular = 1;
+        material.shininess = 300;
+        material.castsShadow = false;
     }
 }
